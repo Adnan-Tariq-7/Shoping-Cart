1d53ad96b632f850ca09fca44da9a9caa7c46ab1
Adnan Tariq <adnantariq885@gmail.com>
2024-06-09 11:27:45 +0500

    update footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='p-10 bg-primary'>
-        <p className='text-center text-white'>CopyRight &copy; Adnan Tariq. All rights reserved.</p>
+    <footer className='p-5 md:p-10 bg-primary'>
+        <p className='max-md:text-sm text-center text-white'>CopyRight &copy; Adnan Tariq. All rights reserved.</p>
     </footer>
   )
 }
